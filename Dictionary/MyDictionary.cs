7be8b3ad8736814decb2a7add12019079ffd69c4
Mehmet Arpacı <arpaci_mehmet@outlook.com>
2021-01-23 19:08:24 +0200

    23 January

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] keyArray = keys;
+            V[] valueArray = values;
+
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+            for (int i = 0; i < keyArray.Length; i++)
+            {
+                for (int j = 0; j < valueArray.Length; j++)
+                {
+                    keys[i] = keyArray[i];
+                    values[j] = valueArray[j];
+                }
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+            public int Count
+        {
+            get { return keys.Length; }
+        }
+
+
+
+    }
+
+    }
+
---
+++ Dictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            ogrenci.Add(1, "Ahmet");
+            ogrenci.Add(2, "Elif");
+            Console.WriteLine(ogrenci.Count);
+
+            MyDictionary<int, string> ogrenci2 = new MyDictionary<int, string>();
+            ogrenci.Add(1, "Mehmet");
+            ogrenci.Add(2, "Salih");
+            ogrenci.Add(3, "Fatih");
+            Console.WriteLine(ogrenci.Count);
+
+        }
+    }
+}
